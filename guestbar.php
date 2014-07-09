d1f13647b814b5d9265e04a978ebd5cfb78e1172
Milmor <Milmor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-09 08:04:04 +0000

    first commit, v 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-guest-bar/trunk@945429 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ guestbar.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: WP Guest Bar
+Description: Adds a BuddyPress-like guest bar to your WordPress site!
+Version: 1.0
+Author: Marco Milesi
+Author Email: milesimarco@outlook.com
+License:
+Copyright 2013 Marco Milesi (milesimarco@outlook.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// functions and options will be added in the future
+function wpgb_login_adminbar( $wp_admin_bar) {
+	if ( !is_user_logged_in() ) {
+		$wp_admin_bar->add_menu( array( 'title' => __( 'Log In' ), 'href' => wp_login_url() ) );
+		$wp_admin_bar->add_menu( array( 'title' => __( 'Register' ), 'href' => wp_registration_url() ) );
+	}
+}
+add_action( 'admin_bar_menu', 'wpgb_login_adminbar' );
+add_filter( 'show_admin_bar', '__return_true' , 1000 );
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+﻿=== WP Guest Bar ===
+Contributors: Milmor
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=F2JK36SCXKTE2
+Tags: toolbar, guest, anonymous, admin, bar, wordpress, login, register, buddypress, bbress, community
+Requires at least: 3.3
+Tested up to: 3.6
+Version: 1.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a BuddyPress-like guest bar to your WordPress site!
+
+== Description ==
+Adds a BuddyPress-like guest bar to your WordPress site, giving guest users a link to log-in or register in your website.
+
+An italian demo can be found at [wpgov.it](http://wpgov.it)
+
+This could be useful, for example, with a BBPress installation or just if you want to engage new and old users to log into your website.
+
+WP Guest Bar is a new plugin. More features and customization will be added soon...
+
+If you like WP Guest
+
+== Installation ==
+1. Upload the entire folder to the '/wp-content/plugins/' directory or install direcly via the WordPress plugin screen
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enjoy!
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0 - 09.07.2014 =
+* **First** release
